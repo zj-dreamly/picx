770919ff901c7c3f214bfcb54505c0ca0fcdd3e2
zj <liugz@253.com>
2021-08-26 10:40:21 +0800

    fix: 自动设置目录按照日期

--- src/common/utils/uploadUrlHandle.ts
+++ src/common/utils/uploadUrlHandle.ts
@@ -1,9 +1,12 @@
 import { UserConfigInfoModel } from '../model/userConfigInfo.model'
+import TimeHelper from '@/common/utils/timeHelper'
 
 const uploadUrlHandle = (config: UserConfigInfoModel, filename: string): string => {
-  let path = ''
+  const date = TimeHelper.getYyyyMmDd()
+  let path = `${date}/`
   if (config.selectedDir !== '/') {
-    path = config.selectedDir + '/'
+    // path = config.selectedDir + '/'
+    path = `${date}/`
   }
   return `/repos/${config.owner}/${config.selectedRepos}/contents/${path}${filename}`
 }
